16b0f2798b9762e84c458efafe987b82c454c951
Umar Farooq-Ansari <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>
2023-08-31 10:55:24 +0500

    ok

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          screens: {
+            'sm': '640px',    // Small screens (default: 640px)
+            'md': '768px',    // Medium screens (default: 768px)
+            'lg': '1024px',   // Large screens (default: 1024px)
+            'xl': '1280px',   // Extra large screens (default: 1280px)
+          },
+      },
+
+    },
+  },
+  plugins: [],
+}
